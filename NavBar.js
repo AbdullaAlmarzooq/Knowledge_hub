065577eeb1dd3bd243e350ccc997b8c3b256afb8
Your Name <abdulla.marzooq@hotmail.com>
2025-05-03 18:31:48 +0300

    Label update "Contacts"

--- NavBar.js
+++ NavBar.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
       <li><a href="Official_documents.html"><i class="fas fa-file-alt"></i> Official Documents</a></li>
       <li><a href="Service_Guide.html"><i class="fas fa-book"></i> Service Guide</a></li>
       <li><a href="Links.html"><i class="fas fa-link"></i> Links</a></li>
-      <li><a href="Contacts.html"><i class="fas fa-address-book"></i> Contact numbers</a></li>
+      <li><a href="Contacts.html"><i class="fas fa-address-book"></i> Contact details</a></li>
     </ul>
   `;
 });
