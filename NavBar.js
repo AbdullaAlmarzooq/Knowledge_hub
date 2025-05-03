528def1407c443e3d918de6862e55e94d6af9a62
Your Name <abdulla.marzooq@hotmail.com>
2025-05-03 10:53:37 +0300

    icons for navigation bar

--- NavBar.js
+++ NavBar.js
@@ -1,12 +1,12 @@
+
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('nav-placeholder').innerHTML = `
-      <ul style="list-style-type: none; padding: 0; margin: 0;">
-        <li style="margin-bottom: 10%;"><a href="Main_page.html" style="text-decoration: none; color: inherit;">Main page</a></li>
-        <li style="margin-bottom: 10%;"><a href="Official_documents.html" style="text-decoration: none; color: inherit;">Official Documents</a></li>
-        <li style="margin-bottom: 10%;"><a href="Service_Guide.html" style="text-decoration: none; color: inherit;">Service Guide</a></li>
-        <li style="margin-bottom: 10%;"><a href="Links.html" style="text-decoration: none; color: inherit;">Links</a></li>
-        <li><a href="Contacts.html" style="text-decoration: none; color: inherit;">Contact numbers</a></li>
-      </ul>
-    `;
-  });
-  
+  document.getElementById('nav-placeholder').innerHTML = `
+    <ul>
+      <li><a href="Main_page.html"><i class="fas fa-home"></i> Main page</a></li>
+      <li><a href="Official_documents.html"><i class="fas fa-file-alt"></i> Official Documents</a></li>
+      <li><a href="Service_Guide.html"><i class="fas fa-book"></i> Service Guide</a></li>
+      <li><a href="Links.html"><i class="fas fa-link"></i> Links</a></li>
+      <li><a href="Contacts.html"><i class="fas fa-address-book"></i> Contact numbers</a></li>
+    </ul>
+  `;
+});
